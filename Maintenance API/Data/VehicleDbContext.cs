b088cf1f7ed284c7a74383ea061a0a3378e02b2e
DerSalik <mahamadarbil44@gmail.com>
2023-05-09 21:04:38 +0300

    Add project files.

---
+++ Maintenance API/Data/VehicleDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Maintenance_API.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace Maintenance_API.Data
+{
+    public class VehicleDbContext:DbContext
+    {
+        public VehicleDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<Vehicle> Vehicles { get; set; }
+    }
+}
