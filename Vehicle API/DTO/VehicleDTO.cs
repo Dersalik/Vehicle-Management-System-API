77c173a8f9cc8d0ae78ae643410c3dc7d20b8486
DerSalik <mahamadarbil44@gmail.com>
2023-05-10 10:30:35 +0300

    .

--- Vehicle API/DTO/VehicleDTO.cs
+++ Vehicle API/DTO/VehicleDTO.cs
@@ -1,6 +1,16 @@
-﻿namespace Vehicle_API.DTO
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Vehicle_API.DTO
 {
     public class VehicleDTO
     {
+        [Required]
+        public string Make { get; set; }
+        [Required]
+        public string Model { get; set; }
+        [Required]
+        public DateTime year { get; set; }
+        [Required]
+        public string VIN { get; set; }
     }
 }
